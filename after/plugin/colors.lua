6417ede6a4625b697d1761cab73e4bdbeaddf3ee
zuidec <case@CASE-DEKSTOP>
2023-11-07 10:50:00 -0800

    Uncommented lines to make background transparent

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,8 +2,8 @@ function ColorMyPencils(color)
 	color = color or "PaperColor"
 	vim.cmd.colorscheme(color)
 
---	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---	vim.api.nvim_set_hl(0, "NormalFloatNormal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloatNormal", { bg = "none" })
 end
 
 ColorMyPencils()
