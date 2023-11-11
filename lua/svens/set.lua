049a1a84f6d0ab53ac6d551e4550fee8f15250d6
zuidec <case@CASE-DEKSTOP>
2023-11-10 18:40:50 -0800

    Fixed tabs and made cursor blink

--- lua/svens/set.lua
+++ lua/svens/set.lua
@@ -1,5 +1,6 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
@@ -13,7 +14,7 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt. termguicolors = true
+vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
@@ -23,4 +24,4 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
-vim.g.mapleader = " "
+vim.opt.guicursor = "n-v-c-sm:block,ci-ve:ver25,r-cr-o:hor20,n-i:block-blinkwait700-blinkoff400-blinkon250-Cursor/lCursor"
