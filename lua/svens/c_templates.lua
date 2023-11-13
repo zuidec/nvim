b396ff3964a9de9d38e8e55d36cbb14c1a659ffe
zuidec <case@CASE-DEKSTOP>
2023-11-13 12:28:34 -0800

    Added more c templating functions

--- lua/svens/c_templates.lua
+++ lua/svens/c_templates.lua
@@ -1,24 +1,73 @@
-function Addguard()
+-- user variable to print into templates
+local user = "zuidec"
+
+function C_headerguard()
     -- Inserts C style header guards into top and bottom of file    
     local filename = vim.fn.expand('%:t')
     filename = string.upper(filename)
     filename = string.gsub(filename, "%.", "_")
+    
+    local info_size = C_fileinfo()
 
-    vim.api.nvim_buf_set_lines(0, 0, 0, true, {
+    vim.api.nvim_buf_set_lines(0, info_size, info_size, true, {
+        "",
         "#ifndef " .. filename,
-        "#define " .. filename,
+        "#define " .. filename .. "\t// BEGIN " .. filename,
+        "",
+        "/*",
+        " *\tIncludes",
+        " */",
+        "",
+        "",
+        "",
+        "/*",
+        " *\tDefines",
+        " */",
+        "",
+        "",
+        "",
+        "/*",
+        " *\tStructs, unions, etc...",
+        " */",
+        "",
+        "",
+        "",
+        "/*",
+        " *\tFunction prototypes",
+        " */",
+        "",
+        "",
         ""
     })
-    vim.api.nvim_buf_set_lines(0, -1, -1, true, {"#endif",""})
+    vim.api.nvim_buf_set_lines(0, -1, -1, true, {"#endif\t// END " .. filename})
 end
 
-function Ctemplate()
+function C_main()
     -- Inserts a basic template with a main() function
-    vim.api.nvim_buf_set_lines(0, 0, 0, true, {
+    
+    local info_size = C_fileinfo()
+    
+    vim.api.nvim_buf_set_lines(0, info_size, info_size, true, {
         "",
         "int main(int argc, char* argv[])   { ",
         "",
         "   return 0;",
         "}"
     })
+end
+
+function C_fileinfo()
+
+    -- Inserts comment at the top of file with name, date
+    local filename = vim.fn.expand('%:t')
+    
+    vim.api.nvim_buf_set_lines(0, 0, 0, true, {
+        "/*",
+        " *\t" .. filename,
+        " *\t(enter file description here)",
+        " *",
+        " *\tCreated by " .. user .. " on " .. os.date('%D'),
+        " */"
+    })
+    return 6
 end
--- lua/svens/functions.lua
+++ lua/svens/functions.lua
@@ -3,15 +3,23 @@ require("svens.c_templates")
 vim.api.nvim_create_user_command('Cguard',
 
     function()
-        Addguard()
+        C_headerguard()
     end,
-    {nargs = 0, desc = 'Add C style header guards'}
+    {nargs = 0, desc = 'Insert .h template with C style header guards'}
 )
 
 vim.api.nvim_create_user_command('Cmain',
 
     function()
-        Ctemplate()
+        C_main()
     end,
-    {nargs = 0, desc = 'Add C style header guards'}
+    {nargs = 0, desc = 'Insert basic .c template with main()'}
+)
+
+vim.api.nvim_create_user_command('Cinfo',
+
+    function()
+        C_fileinfo()
+    end,
+    {nargs = 0, desc = 'Insert comment with file info at top'}
 )
