eb1b458525cd3563788524e47c5043d01277ef51
zuidec <svenstrikesback@gmail.com>
2024-06-18 15:43:07 -0700

    Remove extra spaces at end of line

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,4 +1,4 @@
-local lsp_zero = require('lsp-zero') 
+local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
