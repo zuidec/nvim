a716e6e08a5f46eacf4625497098e44b134a2d6a
zuidec <svenstrikesback@gmail.com>
2025-07-22 23:47:29 -0700

    Update spell file Update deprecated lsp functions

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,5 @@
 local lsp_zero = require('lsp-zero')
-lsp_zero.on_attach(function(client, bufnr)
+lsp_zero:on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
   --lsp_zero.default_keymaps({buffer = bufnr})
@@ -9,8 +9,8 @@ lsp_zero.on_attach(function(client, bufnr)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.jump({count=1, float=true}) end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.jump({count=-1, float=true}) end, opts)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
