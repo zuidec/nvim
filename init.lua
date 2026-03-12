bf44bdc5fa027d8bfda8b1b0384dfcddb49e9573
zuidec <svenstrikesback@gmail.com>
2026-03-11 23:39:32 -0700

    Change colorscheme to evergarden

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
 require("svens")
 local home_dir = os.getenv("HOME")
 package.path = home_dir ..  "/.config/nvim/after/plugin/?.lua"
-require("svens.colors").set_color()
+require("svens.colors").set_color('everforest')
